7b5cb7cdc58692a7f5f2404d6338aafff9ccabf2
Aaron Shafovaloff <aaronshaf@gmail.com>
2018-10-16 23:31:02 -0600

    remove comment

--- set-version.js
+++ set-version.js
@@ -4,7 +4,6 @@ const fs = require("fs");
 async function main() {
   const url = "https://unpkg.com/lit-html/package.json";
   const response = await got(url, { json: true });
-  // console.log(response.body.version);
   const package = require("./package.json");
   package.version = response.body.version;
   fs.writeFileSync(
